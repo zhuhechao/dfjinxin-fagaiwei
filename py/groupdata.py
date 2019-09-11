71c57d587eedc228fcf3ac63bd19f8709ec3bf45
bourne <kuibobo@163.com>
2019-09-11 15:54:52 +0800

    java 调用py 代码

--- py/groupdata.py
+++ py/groupdata.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
+import os
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv('tempdata.csv',header=None,parse_dates=True)#,index_col='日期'
+data = pd.read_csv(os.path.dirname(__file__) + '/tempdata.csv',header=None,parse_dates=True)#,index_col='日期'
 data.columns = ['0','商品', '指标', '环节', '地区', '取值', '日期']
 
 data_group = data.groupby(['日期','指标','地区','商品'])['取值'].mean().unstack().unstack().unstack()#.shape#['1'].value_counts()
 
-data_group.to_csv('data_group.csv')
+data_group.to_csv(os.path.dirname(__file__) + '/data_group.csv')
 
 #
 print(data_group[29][['上海','北京']])
---
+++ src/test/java/io/dfjinxin/modules/sys/service/impl/TestPy.java
@@ -0,0 +1,52 @@
+package io.dfjinxin.modules.sys.service.impl;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 测试Python语言
+ *
+ * @author bourne
+ * @email kuibobo@gmail.com
+ * @date 2019-09-05 17:22:40
+ */
+public class TestPy {
+
+    @Test
+    public void demo1() throws IOException, InterruptedException {
+        String file = new File("").getCanonicalPath() + "\\py\\demo.py";
+
+        int a = 3;
+        int b = 5;
+        String[] args = new String[]{"python", file, String.valueOf(a), String.valueOf(b)};
+        Process proc = Runtime.getRuntime().exec(args);// 执行py文件
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line = null;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        in.close();
+        proc.waitFor();
+    }
+
+    @Test
+    public void demo2() throws IOException, InterruptedException {
+        String file = new File("").getCanonicalPath() + "\\py\\groupdata.py";
+
+        String[] args = new String[]{"python", file};
+        Process proc = Runtime.getRuntime().exec(args);// 执行py文件
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream(),  "gbk"));
+        String line = null;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        in.close();
+        proc.waitFor();
+    }
+}
