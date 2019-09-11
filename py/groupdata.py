3425fcb634c595469a2d7a4f76c92787409de3e8
bourne <kuibobo@163.com>
2019-09-11 09:22:35 +0800

    添加 python 相关代码

---
+++ py/groupdata.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+
+data = pd.read_csv('tempdata.csv',header=None,parse_dates=True)#,index_col='日期'
+data.columns = ['0','商品', '指标', '环节', '地区', '取值', '日期']
+
+data_group = data.groupby(['日期','指标','地区','商品'])['取值'].mean().unstack().unstack().unstack()#.shape#['1'].value_counts()
+
+data_group.to_csv('data_group.csv')
+
+#
+print(data_group[29][['上海','北京']])
