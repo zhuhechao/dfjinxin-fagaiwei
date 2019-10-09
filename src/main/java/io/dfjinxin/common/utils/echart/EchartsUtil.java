10c5b1dd4ec10928bb905f8d4a0a6d7b928631ac
miaoxuanqin <miao+++123>
2019-10-09 18:03:04 +0800

    调度报告任务及模板生成，转换工具

---
+++ src/main/java/io/dfjinxin/common/utils/echart/EchartsUtil.java
@@ -0,0 +1,88 @@
+package io.dfjinxin.common.utils.echart;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.TemplateException;
+import org.apache.http.client.ClientProtocolException;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import sun.misc.BASE64Decoder;
+
+public class EchartsUtil {
+    private static String url = "http://localhost:6666";
+    private static final String SUCCESS_CODE = "1";
+
+    public static String generateEchartsBase64(String option) throws ClientProtocolException, IOException {
+        String base64 = "";
+        if (option == null) {
+            return base64;
+        }
+        option = option.replaceAll("\\s+", "").replaceAll("\"", "'");
+
+        // 将option字符串作为参数发送给echartsConvert服务器
+        Map<String, String> params = new HashMap<>();
+        params.put("opt", option);
+        String response = HttpUtil.post(url, params, "utf-8");
+
+        // 解析echartsConvert响应
+        JSONObject responseJson = JSON.parseObject(response);
+        String code = responseJson.getString("code");
+
+        // 如果echartsConvert正常返回
+        if (SUCCESS_CODE.equals(code)) {
+            base64 = responseJson.getString("data");
+        }
+        // 未正常返回
+        else {
+            String string = responseJson.getString("msg");
+            throw new RuntimeException(string);
+        }
+
+        return base64;
+    }
+
+
+    public static void generateImage(String base64, String path) throws IOException {
+        BASE64Decoder decoder = new BASE64Decoder();
+        try (OutputStream out = new FileOutputStream(path)){
+            // 解密
+            byte[] b = decoder.decodeBuffer(base64);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {
+                    b[i] += 256;
+                }
+            }
+            out.write(b);
+            out.flush();
+        }
+    }
+
+    public static void main(String[] args) throws ClientProtocolException, IOException, TemplateException {
+        // 变量
+        String title = "水果";
+        String[] categories = new String[] { "苹果", "香蕉", "西瓜" };
+        int[] values = new int[] { 3, 2, 1 };
+
+        // 模板参数
+        HashMap<String, Object> datas = new HashMap<>();
+        datas.put("categories", JSON.toJSONString(categories));
+        datas.put("values", JSON.toJSONString(values));
+        datas.put("title", title);
+
+        // 生成option字符串
+        String option = FreemarkerUtil.generateString("option2.ftl", "/template", datas);
+
+        // 根据option参数
+        String base64 = EchartsUtil.generateEchartsBase64(option);
+
+        System.out.println("BASE64:" + base64);
+        generateImage(base64, "E:/UserDirectory/test.png");
+    }
+
+
+}
---
+++ src/main/java/io/dfjinxin/common/utils/word/WordToHtml.java
@@ -0,0 +1,63 @@
+package io.dfjinxin.common.utils.word;
+
+import java.io.*;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.PicturesManager;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.Picture;
+import org.apache.poi.hwpf.usermodel.PictureType;
+import org.apache.poi.xwpf.converter.core.BasicURIResolver;
+import org.apache.poi.xwpf.converter.core.FileImageExtractor;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.w3c.dom.Document;
+
+/**
+ * <p>Title:Word2007ToHtml </p>
+ * <p>Company: </p>
+ * @author
+ * @date 2018年4月11日下午2:21:30
+ * Description:
+ */
+public class WordToHtml {
+
+    public static String word2007ToHtml(String sourceFileName,String reportResultPath) throws Exception {
+        //String filepath = "E:\\ideaWorkspace\\dfjinxin-fagaiwei\\fagaiwei\\2019\\9\\29\\";
+        //String sourceFileName =filepath+"模板名称.docx";
+        String targetFileName =sourceFileName.replace("docx","html");
+        String imagePathStr = reportResultPath+"/image/";
+        OutputStreamWriter outputStreamWriter = null;
+        try {
+          XWPFDocument document = new XWPFDocument(new FileInputStream(sourceFileName));
+          XHTMLOptions options = XHTMLOptions.create();
+          // 存放图片的文件夹
+          options.setExtractor(new FileImageExtractor(new File(imagePathStr)));
+          // html中图片的路径
+          options.URIResolver(new BasicURIResolver("image"));
+          outputStreamWriter = new OutputStreamWriter(new FileOutputStream(targetFileName), "utf-8");
+          XHTMLConverter xhtmlConverter = (XHTMLConverter) XHTMLConverter.getInstance();
+          xhtmlConverter.convert(document, outputStreamWriter, options);
+        } finally {
+          if (outputStreamWriter != null) {
+            outputStreamWriter.close();
+          }
+        }
+        return targetFileName;
+      }
+
+    public static void main(String[] args) throws Throwable {
+
+        //WordToHtml.word2007ToHtml();
+    }
+}
