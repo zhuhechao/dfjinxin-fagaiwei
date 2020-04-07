5442401fcb24a7caa415b825b596ad6f9a4ee6bf
flymail1987 <flymail1987@126.com>
2020-04-07 14:19:48 +0800

    系统优化 add by zhc 2020/4/3

--- src/main/java/io/dfjinxin/common/govcafilter/GovCaFilter.java
+++ src/main/java/io/dfjinxin/common/govcafilter/GovCaFilter.java
@@ -3,9 +3,9 @@ package io.dfjinxin.common.govcafilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.servlet.*;
 import java.io.IOException;
+
 
 public class GovCaFilter implements Filter {
 
@@ -15,7 +15,7 @@ public class GovCaFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding("gbk");//赶在被utf8之前……先gbk了
         servletRequest.getParameter("BJCA_TICKET");
-        filterChain.doFilter(servletRequest,servletResponse);
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {
